101070f98517b4b64cb1222ff202f64e4ae988fa
Kim Gardner <kgardnr@gmail.com>
2015-06-07 23:59:56 -0400

    First draft of game, WIP

---
+++ tictactoe.rb
@@ -0,0 +1,60 @@
+require 'pry'
+
+def initialize_board
+  b = {}
+  (1..9).each { |square| b[square] = " " }
+  b
+end
+
+def draw_board(b)
+  system "clear"
+  puts " #{b[1]} | #{b[2]} | #{b[3]} "
+  puts "-----------"
+  puts " #{b[4]} | #{b[5]} | #{b[6]} "
+  puts "-----------"
+  puts " #{b[7]} | #{b[8]} | #{b[9]} "
+end
+
+def empty_squares(b)
+  b.select { |k, v| v == " " }.keys
+end
+
+def player_chooses_square(b)
+  puts "What's your move? (1-9):"
+  square = gets.chomp.to_i
+  b[square] = "X"
+end
+
+def computer_chooses_square(b)
+  square = empty_squares(b).sample
+  b[square] = "O"
+end
+
+def check_winner(b)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+  winning_lines.each do |line|
+    if b[line[0]] == "X" and b[line[1]] == "X" and b[line[2]] == "X"
+      return "Player"
+    elsif b[line[0]] == "O" and b[line[1]] == "O" and b[line[2]] == "O"
+      return "Computer"
+    else
+      return nil
+    end
+  end
+end
+
+board = initialize_board
+draw_board(board)
+
+begin
+  player_chooses_square(board)
+  computer_chooses_square(board)
+  draw_board(board)
+  winner = check_winner(board)
+end until winner || empty_squares(board).empty?
+
+if winner
+  puts "#{winner} won!"
+else
+  puts "It's a tie!"
+end
